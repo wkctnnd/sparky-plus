8ce3ab3704fe8520f36c55e1ac9d62e3dd03f0a2
sy_Zhou <sy_Zhou@pku.com>
2020-04-13 22:38:11 +0800

    fbx

--- Sparky-core/animation/Interpolator.h
+++ Sparky-core/animation/Interpolator.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <string>
+namespace sparky
+{
+	namespace animation
+	{
+		template<class T>
+		class IInterpolator
+		{
+			virtual T Evaluate(unsigned long duration) = 0;
+
+		};
+
+		template<class T>
+		class LinearInterpolator :public IInterpolator<T>
+		{
+			virtual T Evaluate()
+		};
+	}
+}
--- Sparky-core/animation/sequence.h
+++ Sparky-core/animation/sequence.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <string>
+namespace sparky
+{
+	namespace animation
+	{
+		template<class T>
+		class Sequence
+		{
+			virtual T Evaluate(unsigned long duration) = 0;
+
+		};
+
+		template<class T>
+		class LinearInterpolator :public Interpolator<T>
+		{
+			virtual T Evaluate()
+		};
+	}
+}
