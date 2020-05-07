e8fd29bbda8414265c7453166bb4059ded9aadff
wangkaichen <wkctnnd@sina.com>
2020-05-08 01:38:42 +0800

    ecs

--- Sparky-core/world/ecs/component.h
+++ Sparky-core/world/ecs/component.h
@@ -1,13 +1,18 @@
 #pragma once
+
+//https://zhuanlan.zhihu.com/p/106946506
+
+
 namespace sparky
 {
 	namespace ecs
 	{
-		struct IComponent
+		class IComponent
 		{
+		public:
+
+
 			
-
-
 		};
 	}
 }
