fb2885082a47f9d64903856e5a8ff68b52de23a5
sy_Zhou <sy_Zhou@pku.com>
2020-05-09 23:17:14 +0800

    ecs

---
+++ Sparky-core/world/ecs/EntityManager.h
@@ -0,0 +1,20 @@
+#pragma once
+//http://blog.lmorchard.com/2013/11/27/entity-component-system/
+//https://www.gamasutra.com/blogs/TobiasStein/20171122/310172/The_EntityComponentSystem__An_awesome_gamedesign_pattern_in_C_Part_1.php#refs
+#include <vector>
+namespace sparky
+{
+	namespace ecs
+	{
+		class EnityManager
+		{
+
+
+		private:
+			std::vector<class Entity*> m_EntityArray;
+
+			std::vector<>
+		};
+
+	}
+}
