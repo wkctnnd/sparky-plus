66b28d695869e4f12add235a32f73ca70747efe4
wkctnnd <wkctnnd@sina.com>
2020-11-08 23:43:21 +0800

    弹簧质点

--- Sparky-core/physics/PxSoftBody.h
+++ Sparky-core/physics/PxSoftBody.h
@@ -4,6 +4,7 @@
 #include "maths/AABox.h"
 #include "PxObject.h"
 using namespace sparky::maths;
+//https://blog.csdn.net/ZJU_fish1996/article/details/53056109?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.pc_relevant_is_cache&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.pc_relevant_is_cache
 namespace sparky
 {
 	namespace phyx
